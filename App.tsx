97db664941159c21646d4cc8b7f28f41e48159d9
werlensantos <werlencarsts@gmail.com>
2025-02-05 11:40:49 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useRef } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy
+} from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const mapRef = useRef<MapView>(null); 
+
+  async function requestLocationPermissions() {
+    const {granted} = await requestForegroundPermissionsAsync();
+  
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+      console.log("LOCALIZÇÃO ATUAL: ", currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1
+    }, (response) => {
+      setLocation(response);
+      mapRef.current?.animateCamera({
+        pitch: 40,
+        center: response.coords
+      })
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text  style={styles.headerText}>Mapa de localização atual</Text>
+      </View>
+      {location ? (
+        <MapView
+          ref={mapRef} 
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude
+            }}
+            title="Você está aqui!"
+            description="Essa é a sua localização atual."
+          />
+        </MapView>
+      ) : (
+        <Text>Carregando mapa...</Text>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    padding: 10,
+    paddingTop: 40,
+    backgroundColor: 'blue',
+    width: '100%',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 20
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+  }
+});
