170c87d631c4d8df604348b40a5c02461dcadc75
werlensantos <werlencarsts@gmail.com>
2025-02-07 15:20:20 -0300

    test props maps

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
+import { Linking, Platform } from 'react-native';
 import { useEffect, useState, useRef } from 'react';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Circle, Marker, Polygon, Polyline} from 'react-native-maps';
 import { Text, View } from 'react-native';
 import { styles } from './styles';
 import {
@@ -10,9 +12,36 @@ import {
   LocationAccuracy
 } from 'expo-location';
 
-export default function App() {
+const BH_CENTER = {
+  latitude: -19.922731,
+  longitude: -43.945094,
+};
+
+const coordinates = [
+  { latitude: -19.922731, longitude: -43.945094 }, // Praça Sete (Ponto central)
+  { latitude: -19.918383, longitude: -43.938919 }, // Mercado Central
+  { latitude: -19.921399, longitude: -43.937744 }, // Palácio das Artes
+  { latitude: -19.924739, longitude: -43.936502 }, // Edifício Maletta
+  { latitude: -19.922731, longitude: -43.945094 }, // Retorno ao ponto inicial (Praça Sete)
+  
+];
+const destinationDate = {
+  "coords": {
+    "accuracy": 4.348532508071283,
+    "altitude": 860.3961109733209,
+    "altitudeAccuracy": 21.612985000444276,
+    "heading": -1,
+    "latitude": -19.917,
+    "longitude": -43.933,
+    "speed": 0
+  },
+  "timestamp": 1738947761666.3015
+}
+
+export default function App(): JSX.Element {
   const [location, setLocation] = useState<LocationObject | null>(null);
   const mapRef = useRef<MapView>(null); 
+  const [destination, setDestination] = useState<LocationObject | null>(destinationDate);
 
   // Function to request location permissions
   async function requestLocationPermissions() {
@@ -21,7 +50,7 @@ export default function App() {
     if (granted) {
       const currentPosition = await getCurrentPositionAsync();
       setLocation(currentPosition);
-      console.log("LOCALIZÇÃO ATUAL: ", currentPosition);
+      console.log("LOCALIZAÇÃO ATUAL: ", currentPosition);
     }
   }
 
@@ -44,6 +73,16 @@ export default function App() {
       })
     })
   }, [])
+
+  // Function to open App Google Maps
+  const openMaps = (latitude: Number, longitude: Number) => {
+    const url = Platform.select({
+      ios: `maps:0,0?q=${latitude},${longitude}`,
+      android: `google.navigation:q=${latitude},${longitude}`,
+    });
+  
+    Linking.openURL(url).catch(err => console.error('Error opening map:', err));
+  };
 
   return (
     <View style={styles.container}>
@@ -69,6 +108,31 @@ export default function App() {
             title="Você está aqui!"
             description="Essa é a sua localização atual."
           />
+
+          <Marker
+            onPress={() => openMaps(destination.coords.latitude, destination.coords.longitude)}
+            coordinate={{
+              latitude: destination.coords.latitude,
+              longitude: destination.coords.longitude
+            }}
+            title="Seu destino!"
+            description="Essa é localização do seu destino."
+            image={require('./assets/icon-map.png')}
+          />
+
+          <Polyline
+            coordinates={coordinates}
+            strokeColor="#000" // Cor da linha
+            strokeWidth={3}    // Largura da linha
+          />
+
+        <Circle
+          center={BH_CENTER}
+          radius={450} // Raio de 1km em metros
+          strokeWidth={2}
+          strokeColor="rgb(251, 157, 3)" // Azul semi-transparente
+          fillColor="rgba(251, 157, 3, 0.5)" // Azul claro semi-transparente
+        />
         </MapView>
       ) : (
         <Text>Carregando mapa...</Text>
